268f4308b2f80729aeb03bff11867e85a1aec12e
natiArgento <nati.argento51@gmail.com>
2014-10-12 13:13:45 -0300

    agrego formulario de alta habitacion

---
+++ Aplicacion Desktop/FrbaHotel/FrbaHotel/ABM de Habitacion/AltaHabitacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrbaHotel.ABM_de_Habitacion
+{
+    public partial class AltaHabitacion : Form
+    {
+        public AltaHabitacion()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
