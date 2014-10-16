8c15b9fe511b3b7b2ece1efcbb3c7ae619e3236e
emiiSierra <sierraemii@gmail.com>
2014-10-16 12:59:49 -0300

    Vuelve el Alta habitacion al proyecto original

--- Aplicacion Desktop/FrbaHotel/FrbaHotel/Login/FrmLogin.cs
+++ Aplicacion Desktop/FrbaHotel/FrbaHotel/Login/FrmLogin.cs
@@ -24,25 +24,41 @@ namespace FrbaHotel.Login
 
         private void CmdIngresar_Click(object sender, EventArgs e)
         {
+            //viejoLogin();
+            BD bd = new BD();
+            SqlConnection conexion = bd.obtenerConexion();
+            string comando = "SELECT Username,Contraseña FROM FUGAZZETA.Usuarios WHERE Username='" + TxtUser.Text + "'";
+            DataTableReader tabla = new DataTableReader(bd.ejecutar(comando));
+            if (tabla.HasRows)
+            {
+                tabla.Read();
+                string mensaje = tabla[0].ToString();
+                MessageBox.Show(mensaje);
+            }
+
+        }
+
+        private void viejoLogin()
+        {
             BD bd = new BD();
             SqlConnection conexion = bd.obtenerConexion();
             SqlCommand cmd = new SqlCommand();
             cmd.CommandText = "FUGAZZETA.ValidarLogin";
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Connection = conexion;
-            cmd.Parameters.AddWithValue("USER2",TxtUser.Text);
-            cmd.Parameters.AddWithValue("PASS2",TxtPass.Text);
-            
+            cmd.Parameters.AddWithValue("USER2", TxtUser.Text);
+            cmd.Parameters.AddWithValue("PASS2", TxtPass.Text);
+
             try
             {
                 Int32 reader = cmd.ExecuteNonQuery();
                 bd.cerrar();
-            } catch (SqlException ex)
+            }
+            catch (SqlException ex)
             {
                 LblError.Text = ex.Number.ToString();
             }
             MessageBox.Show("Login correcto");
-
         }
 
     }
