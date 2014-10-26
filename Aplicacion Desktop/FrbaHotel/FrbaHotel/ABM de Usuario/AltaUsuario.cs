f768f3e3e9669f0922453ebccae291663a22e38b
natiArgento <nati.argento51@gmail.com>
2014-10-26 17:14:55 -0300

    cambio nombre de id en buscador, ahora abre

--- Aplicacion Desktop/FrbaHotel/FrbaHotel/ABM de Usuario/AltaUsuario.cs
+++ Aplicacion Desktop/FrbaHotel/FrbaHotel/ABM de Usuario/AltaUsuario.cs
@@ -20,6 +20,7 @@ namespace FrbaHotel.ABM_de_Usuario
         public AltaUsuario()
         {
             InitializeComponent();
+            InitializeComponent();
         }
         #region Botones
         private void CmdQuitar_Click(object sender, EventArgs e)
