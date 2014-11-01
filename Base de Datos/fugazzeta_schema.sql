36fe65689c904a6c370a8a021b3dbd1b767edcc8
emiiSierra <sierraemii@gmail.com>
2014-11-01 19:58:19 -0300

    Agrego la migracion de reservas y movimientos de reserva. La migracion estaria supuestamente COMPLETA

--- Base de Datos/fugazzeta_schema.sql
+++ Base de Datos/fugazzeta_schema.sql
@@ -66,9 +66,6 @@ DROP TABLE FUGAZZETA.Reservas
 IF OBJECT_ID('FUGAZZETA.EstadosReserva') IS NOT NULL
 DROP TABLE FUGAZZETA.EstadosReserva
 
-IF OBJECT_ID('FUGAZZETA.ClientesDuplicados') IS NOT NULL
-DROP TABLE FUGAZZETA.ClientesDuplicados
-
 IF OBJECT_ID('FUGAZZETA.Clientes') IS NOT NULL
 DROP TABLE FUGAZZETA.Clientes
 
@@ -141,6 +138,9 @@ DROP PROCEDURE FUGAZZETA.OcupacionEnHotelEnPeriodo
 IF OBJECT_ID('FUGAZZETA.CancelarReserva') IS NOT NULL
 DROP PROCEDURE FUGAZZETA.CancelarReserva
 
+IF OBJECT_ID('FUGAZZETA.MigrarReservas') IS NOT NULL
+DROP PROCEDURE FUGAZZETA.MigrarReservas
+
 IF OBJECT_ID('FUGAZZETA.OcupacionEnHotelEnPeriodo') IS NOT NULL
 DROP PROCEDURE FUGAZZETA.OcupacionEnHotelEnPeriodo
 
@@ -156,6 +156,8 @@ DROP PROCEDURE FUGAZZETA.HabitacionesLibresEnPeriodo
 IF OBJECT_ID('FUGAZZETA.GenerarReserva') IS NOT NULL
 DROP PROCEDURE FUGAZZETA.GenerarReserva
 
+IF OBJECT_ID('FUGAZZETA.Migrar_ItemsHospedaje') IS NOT NULL
+DROP PROCEDURE FUGAZZETA.Migrar_ItemsHospedaje
 --Triggers
 IF OBJECT_ID('FUGAZZETA.TR_MovimientosHotel_A_I') IS NOT NULL
 DROP TRIGGER FUGAZZETA.TR_MovimientosHotel_A_I
@@ -401,17 +403,12 @@ BEGIN
 END
 GO
 
-CREATE TRIGGER FUGAZZETA.TR_Reservas_A_U ON FUGAZZETA.Reservas
-AFTER UPDATE AS
-BEGIN
-	DECLARE mi_cursor FOR (SELECT Id_Reserva, 
-
 --------------------------/* Poblado de Datos*/---------------------------------------------
 --------------------------------------------------------------------------------------------
-INSERT INTO FUGAZZETA.Paises values ('Argentina')
-INSERT INTO FUGAZZETA.Paises values ('Uruguay')
-INSERT INTO FUGAZZETA.Paises values ('España')
-INSERT INTO FUGAZZETA.Paises values ('Brasil')
+INSERT INTO FUGAZZETA.Paises values ('ARGENTINA')
+INSERT INTO FUGAZZETA.Paises values ('URUGUAY')
+INSERT INTO FUGAZZETA.Paises values ('ESPAÑA')
+INSERT INTO FUGAZZETA.Paises values ('BRASIL')
 go
 
 --Funcionalidades
@@ -499,8 +496,6 @@ UPDATE FUGAZZETA.[Usuarios x Hoteles x Rol] SET Id_Rol = 1 where Username = 'adm
 UPDATE FUGAZZETA.[Usuarios x Hoteles x Rol] SET	Id_Rol = 4 where Username = 'guest'
 GO
 
-
-
 INSERT INTO FUGAZZETA.Regimenes
 (Precio,Descripcion)
 SELECT DISTINCT
@@ -526,9 +521,68 @@ INSERT INTO	FUGAZZETA.EstadosReserva values('Cancelada por No-Show')
 INSERT INTO	FUGAZZETA.EstadosReserva values('Efectivizada')
 go
 
---RESERVA
+CREATE PROCEDURE FUGAZZETA.MigrarReservas AS
+BEGIN
+SELECT DISTINCT
+Reserva_Codigo, Cliente_Pasaporte_Nro, Cliente_Apellido, Hotel_Calle, Hotel_Nro_Calle,
+cast(DATEADD(d,Reserva_Cant_Noches,Reserva_Fecha_Inicio) as DATE) as FinReserva,
+cast(Reserva_Fecha_Inicio as DATE) AS ReservaIngreso,
+cast(Estadia_Fecha_Inicio as DATE) AS Ingreso,
+cast(DATEADD(d,Estadia_Cant_Noches,Estadia_Fecha_Inicio) as DATE) as Egreso,
+Regimen_Descripcion
+INTO #TempReservas FROM gd_esquema.Maestra 
+	
+SET IDENTITY_INSERT FUGAZZETA.Reservas ON
+INSERT INTO FUGAZZETA.Reservas
+(Id_Reserva,Id_Cliente, Id_Hotel,Fecha_Inicio,Fecha_Fin_Reserva,Id_Regimen)
+SELECT
+M.Reserva_Codigo, C.Id_Cliente, H.Id_Hotel, M.ReservaIngreso, M.FinReserva, REG.Id_Regimen
+FROM
+FUGAZZETA.Clientes C, FUGAZZETA.Hoteles H, FUGAZZETA.Regimenes REG,#TempReservas M
+WHERE
+	C.Nro_Doc = M.Cliente_Pasaporte_Nro
+AND C.Apellido = M.Cliente_Apellido
+AND H.Calle = M.Hotel_Calle 
+AND H.Nro_Calle = M.Hotel_Nro_Calle 
+AND REG.Descripcion = M.Regimen_Descripcion
+AND M.Ingreso IS NULL
+AND M.Egreso IS NULL
+SET IDENTITY_INSERT FUGAZZETA.Reservas OFF
 
---MOVIMIENTOSRESERVA
+MERGE INTO FUGAZZETA.Reservas RES
+USING
+(SELECT Reserva_Codigo,Egreso FROM #TempReservas
+WHERE 
+Ingreso IS NOT NULL
+AND Egreso IS NOT NULL) E
+ON RES.Id_Reserva = E.Reserva_Codigo
+WHEN MATCHED THEN UPDATE
+SET Fecha_Egreso = E.Egreso,
+Id_EstadoReserva = 6;
+
+UPDATE FUGAZZETA.Reservas SET Id_EstadoReserva = 1 WHERE Id_EstadoReserva IS NULL
+
+DECLARE mi_cursor CURSOR FOR
+(SELECT Reserva_Codigo,Ingreso, Egreso FROM #TempReservas
+WHERE 
+	Ingreso IS NOT NULL
+AND Egreso IS NOT NULL)
+DECLARE @RESERVA INT
+DECLARE @Ing date
+DECLARE @Egr date
+OPEN mi_cursor	FETCH NEXT FROM mi_cursor INTO @RESERVA, @Ing,@Egr
+WHILE @@FETCH_STATUS = 0
+	BEGIN
+	INSERT INTO FUGAZZETA.MovimientosReserva (Id_Reserva,Proceso,FechaMovimiento,Motivo) values (@RESERVA,'I',@Ing,'Ingreso al hotel')
+	INSERT INTO FUGAZZETA.MovimientosReserva (Id_Reserva,Proceso,FechaMovimiento,Motivo) values (@RESERVA,'E',@Egr,'Egreso del hotel')
+	FETCH NEXT FROM mi_cursor INTO @RESERVA, @Ing, @Egr
+	END
+CLOSE mi_cursor
+DEALLOCATE mi_cursor
+END
+GO
+EXEC FUGAZZETA.MigrarReservas
+GO
 
 --ACOMPAÑANTES: NO HAY INFORMACION DE PERSONAS QUE ACOMPAÑARON EN UNA ESTADIA.
 
@@ -536,11 +590,7 @@ SET IDENTITY_INSERT FUGAZZETA.TiposHabitacion ON
 INSERT INTO FUGAZZETA.TiposHabitacion (Id_TipoHab,Descripcion,Porcentual)
 SELECT DISTINCT Habitacion_Tipo_Codigo,Habitacion_Tipo_Descripcion,Habitacion_Tipo_Porcentual FROM gd_esquema.Maestra
 SET IDENTITY_INSERT FUGAZZETA.TiposHabitacion OFF
-UPDATE FUGAZZETA.TiposHabitacion SET CantPersonas = 1 WHERE Id_TipoHab = 1001
-UPDATE FUGAZZETA.TiposHabitacion SET CantPersonas = 2 WHERE Id_TipoHab = 1002
-UPDATE FUGAZZETA.TiposHabitacion SET CantPersonas = 3 WHERE Id_TipoHab = 1003
-UPDATE FUGAZZETA.TiposHabitacion SET CantPersonas = 4 WHERE Id_TipoHab = 1004
-UPDATE FUGAZZETA.TiposHabitacion SET CantPersonas = 5 WHERE Id_TipoHab = 1005
+UPDATE FUGAZZETA.TiposHabitacion SET CantPersonas = cast(RIGHT(Id_TipoHab,1) as int)
 GO
 
 SET IDENTITY_INSERT FUGAZZETA.Facturas ON
@@ -584,7 +634,7 @@ BEGIN
 				Monto = 0
 			WHERE Ocupadas IS NULL AND CantNoches IS NULL AND MONTO IS NULL
 	COMMIT TRANSACTION
-	-- Después del poblado no se permitirán valores nulos.	
+	-- Después del poblado se agrega la PK en la tabla.	
 END
 GO
 EXEC FUGAZZETA.Migrar_ItemsHospedaje
