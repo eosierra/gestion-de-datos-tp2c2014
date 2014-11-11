d96a3a070d95a2909a62c2bc05b4694be2b22374
Lucas Pugnali <pugnali.lucas@gmail.com>
2014-11-11 13:36:39 -0300

    Agrego listado para Modificar HAbitacion

---
+++ Aplicacion Desktop/FrbaHotel/FrbaHotel/ABM de Habitacion/BuscarHabitacion.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace FrbaHotel.ABM_de_Habitacion
+{
+    partial class BuscarHabitacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtHabitacion = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.GridHabitaciones = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridHabitaciones)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.TxtHabitacion);
+            this.groupBox1.Location = new System.Drawing.Point(21, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(209, 331);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtros de selección";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Habitacion:";
+            // 
+            // TxtHabitacion
+            // 
+            this.TxtHabitacion.Location = new System.Drawing.Point(9, 66);
+            this.TxtHabitacion.Name = "TxtHabitacion";
+            this.TxtHabitacion.Size = new System.Drawing.Size(146, 20);
+            this.TxtHabitacion.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(139, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 34);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "CANCELAR SELECCION";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(274, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 34);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "SELECCIONAR";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // GridHabitaciones
+            // 
+            this.GridHabitaciones.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.GridHabitaciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridHabitaciones.Location = new System.Drawing.Point(246, 12);
+            this.GridHabitaciones.Name = "GridHabitaciones";
+            this.GridHabitaciones.ReadOnly = true;
+            this.GridHabitaciones.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.GridHabitaciones.Size = new System.Drawing.Size(685, 331);
+            this.GridHabitaciones.TabIndex = 6;
+            // 
+            // BuscarHabitacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 400);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.GridHabitaciones);
+            this.Name = "BuscarHabitacion";
+            this.Text = "Buscar Habitacion";
+            this.Load += new System.EventHandler(this.BuscarHabitacion_Load_1);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridHabitaciones)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtHabitacion;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView GridHabitaciones;
+    }
+}
---
+++ Aplicacion Desktop/FrbaHotel/FrbaHotel/ABM de Habitacion/BuscarHabitacion.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+
+namespace FrbaHotel.ABM_de_Habitacion
+{
+    public partial class BuscarHabitacion : Buscador
+    {
+        int idHotel;
+
+        public BuscarHabitacion(ITraeBusqueda owner,int hotel)
+        {
+            InitializeComponent();
+            crearBuscador(owner, "Num_Habitacion", "Habitaciones");
+            setearGrid(GridHabitaciones);
+            idHotel = hotel;
+            
+        }
+
+        private void BuscarHabitacion_Load_1(object sender, EventArgs e)
+        {
+            bd.obtenerConexion();
+            actual = todos;
+            addFiltroPorID(idHotel, "Id_Hotel", GridHabitaciones);
+            cargarGrilla(GridHabitaciones, actual);
+        }
+
+
+        
+
+        /*private void TxtRol_TextChanged(object sender, EventArgs e)
+        {
+            actual = todos;
+            addFiltroTextBox(TxtRol, "Nombre", GridRoles);
+            cargarGrilla(GridRoles, actual);
+
+        }*/
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+        /*private void button1_Click(object sender, EventArgs e)
+        {
+            if (funcion == 'S')
+            {
+                string id = celdaElegida(GridRoles, 0);
+                string name = celdaElegida(GridRoles, 1);
+                Rol rol = new Rol(id, name);
+                dondeVuelve.agregar(id, name);
+                this.Close();
+            }
+            if (funcion == 'M')
+            {
+                string id = celdaElegida(GridRoles, 0);
+                string name = celdaElegida(GridRoles, 1);
+                DialogResult modif = new AltaRol('M', id, name).ShowDialog();
+                if (modif == DialogResult.OK)
+                {
+                    Close();
+                }
+            }
+        }*/
+    }
+}
+       
+
